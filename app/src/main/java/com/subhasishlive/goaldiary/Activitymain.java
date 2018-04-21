0a97fef3f378307d0546499014ec6aa534f4cd8d
subhasishnath45 <subhasishnath45@gmail.com>
2018-04-21 16:44:34 +0530

    Initial commit

---
+++ app/src/main/java/com/subhasishlive/goaldiary/Activitymain.java
@@ -0,0 +1,22 @@
+package com.subhasishlive.goaldiary;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class Activitymain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
+

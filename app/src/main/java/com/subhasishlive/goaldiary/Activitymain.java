dafba53c1db5fdd8df468fed9fc82c97c830fe0e
subhasishnath45 <subhasishnath45@gmail.com>
2018-04-22 15:14:22 +0530

    2nd commit

--- app/src/main/java/com/subhasishlive/goaldiary/Activitymain.java
+++ app/src/main/java/com/subhasishlive/goaldiary/Activitymain.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.text.Editable;
 import android.view.View;
 import android.widget.CheckBox;
@@ -12,10 +13,15 @@ import android.widget.TextView;
 
 public class Activitymain extends AppCompatActivity {
 
+    Toolbar mToolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
     }
 
 }
